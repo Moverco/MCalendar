35d04200e14c2e39284e036a348ad214eca409bc
Moverco <moverco9@gmail.com>
2017-09-28 13:41:36 +0800

    0.0.1
    Add new layout file

---
+++ calender/src/main/java/top/moverco/calender/MCalender.java
@@ -0,0 +1,25 @@
+package top.moverco.calender;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Moverco.
+ */
+
+public class MCalender extends LinearLayout {
+    public MCalender(Context context) {
+        super(context);
+    }
+
+    public MCalender(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MCalender(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
