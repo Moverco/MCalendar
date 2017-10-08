677018cc8d0c56cd4928326d8c6cad1d309733cd
Moverco <moverco9@gmail.com>
2017-10-08 13:41:49 +0800

    0.0.3
    Add demo

--- calendar/src/main/java/top/moverco/calender/MCalendar.java
+++ calendar/src/main/java/top/moverco/calender/MCalendar.java
@@ -1,16 +1,22 @@
 package top.moverco.calender;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Moverco.
@@ -20,9 +26,9 @@ public class MCalendar extends LinearLayout {
 
     private ImageView btnPrev;
     private ImageView btnNext;
-    private TextView txtMonth;
+    private TextView textDate;
     private GridView grid;
-    private Calendar mCalendar;
+    private Calendar curDate = Calendar.getInstance();
 
     public MCalendar(Context context) {
         super(context);
@@ -40,31 +46,74 @@ public class MCalendar extends LinearLayout {
     private void initControl(Context context){
         bindControl(context);
         bindControlEvent();
+        renderCalendar();
     }
 
     private void bindControlEvent() {
         btnPrev.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCalendar.add(C);
+                curDate.add(Calendar.MONTH,-1);
+                renderCalendar();
             }
         });
         btnNext.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                curDate.add(Calendar.MONTH,1);
+                renderCalendar();
             }
         });
     }
 
+    private void renderCalendar() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM yyyy");
+        textDate.setText(simpleDateFormat.format(curDate.getTime()));
+
+        ArrayList<Date> cells = new ArrayList<>();
+        Calendar calendar = (Calendar) curDate.clone();
+
+        calendar.set(Calendar.DAY_OF_MONTH,1);
+        int prevDays = calendar.get(Calendar.DAY_OF_WEEK)-1;
+        calendar.add(Calendar.DAY_OF_MONTH,-prevDays);
+
+        int maxCellsCount = 6*7;
+        while (cells.size() < maxCellsCount){
+            cells.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH,1);
+        }
+        grid.setAdapter(new CalendarAdapter(getContext(),cells));
+    }
+
     private void bindControl(Context context) {
         LayoutInflater infalter = LayoutInflater.from(context);
-        infalter.inflate(R.layout.calendar_view,this,false);
+        infalter.inflate(R.layout.calendar_view,this);
 
-        btnPrev = (ImageView) findViewById(R.id.calender_prev_button);
-        btnNext = (ImageView) findViewById(R.id.calender_next_button);
-        txtMonth = (TextView) findViewById(R.id.calender_header_text);
-        grid = (GridView) findViewById(R.id.calender_grid);
+        btnPrev = (ImageView) findViewById(R.id.calendar_prev_button);
+        btnNext = (ImageView) findViewById(R.id.calendar_next_button);
+        textDate = (TextView) findViewById(R.id.calendar_header_text);
+        grid = (GridView) findViewById(R.id.calendar_grid);
 
+    }
+
+    private class CalendarAdapter extends ArrayAdapter<Date>{
+
+        LayoutInflater mInflater;
+        public CalendarAdapter(@NonNull Context context,ArrayList<Date> days) {
+            super(context,R.layout.calendar_text_day);
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            Date date = getItem(position);
+            if (convertView==null){
+                convertView = mInflater.inflate(R.layout.calendar_text_day,parent,false);
+            }
+            int day = date.getDate();
+            ((TextView)convertView).setText(String.valueOf(day));
+            return convertView;
+        }
     }
 }
